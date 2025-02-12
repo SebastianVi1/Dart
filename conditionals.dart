58a05453d2fc6b92210c07718c7d1779ecc399c0
Sebas <sebastianvh86@gmali.com>
2025-02-11 19:25:29 -0600

    Conditionals

---
+++ conditionals.dart
@@ -0,0 +1,34 @@
+//Normal if
+/* if (isRaining()) {
+  you.bringRainCoat();
+} else if (isSnowing()) {
+  you.wearJacket();
+} else {
+  car.putTopDown();
+} */
+
+
+//if-case 
+/* if (pair case [int x, int y]) {
+  print('Was coordinate array $x,$y');
+} else {
+  throw FormatException('Invalid coordinates.');
+} */
+
+//SWITCH CASE 
+// String command = 'OPEN';
+// switch (command) {
+//   case 'CLOSED':
+//     executeClosed();
+//   case 'PENDING':
+//     executePending();
+//   case 'APPROVED':
+//     executeApproved();
+//   case 'DENIED':
+//     executeDenied();
+//   case 'OPEN':
+//     executeOpen();
+//   default:
+//     executeUnknown();
+// }   
+
